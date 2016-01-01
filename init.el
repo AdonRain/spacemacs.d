bd089db306f4035c4bd1a840c3ea3ea557226d0e
雷东升 <leidongsheng@meituan.com>
2016-01-01 10:00:08 +0800

    delete some themes

--- init.el
+++ init.el
@@ -25,19 +25,15 @@ values."
      ;; ----------------------------------------------------------------
      auto-completion
      emacs-lisp
-     javascript
-     html
-     git
-     ;; markdown
-     org
      eyebrowse
+     git
+     html
+     javascript
+     org
      (shell :variables
             shell-default-shell 'eshell
             shell-default-height 30
             shell-default-position 'bottom)
-     ;; spell-checking
-     ;; syntax-checking
-     ;; version-control
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
@@ -81,13 +77,7 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
-                         spacemacs-light
-                         solarized-light
-                         solarized-dark
-                         leuven
-                         monokai
-                         zenburn)
+   dotspacemacs-themes '(spacemacs-dark)
    ;; If non nil the cursor color matches the state color.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
