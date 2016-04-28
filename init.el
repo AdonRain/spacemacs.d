f40b6c6614c793f4cbb637219ba71367886b664a
雷东升 <leidongsheng@meituan.com>
2016-04-28 18:56:43 +0800

    synchronization dotfile

--- init.el
+++ init.el
@@ -231,15 +231,23 @@ values."
 
 (defun dotspacemacs/user-init ()
   "Initialization function for user code.
-It is called immediately after `dotspacemacs/init'.  You are free to put almost
-any user code here.  The exception is org related code, which should be placed
-in `dotspacemacs/user-config'."
+It is called immediately after `dotspacemacs/init', before layer configuration
+executes.
+ This function is mostly useful for variables that need to be set
+before packages are loaded. If you are unsure, you should try in setting them in
+`dotspacemacs/user-config' first."
   )
 
 (defun dotspacemacs/user-config ()
   "Configuration function for user code.
 This function is called at the very end of Spacemacs initialization after
-layers configuration. You are free to put any user code."
+layers configuration.
+This is the place where most of your configurations should be done. Unless it is
+explicitly specified that a variable should be set before a package is loaded,
+you should place your code here."
+  (setq-default
+   js2-basic-offset 4
+   js-indent-level 4)
   (setq powerline-default-separator 'nil)
   (global-linum-mode))
 
